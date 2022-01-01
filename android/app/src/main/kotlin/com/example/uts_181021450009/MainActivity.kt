879918750d02538170bd5dc090dea1ffec5d151b
David Kurniawan <davidyfsays@gmail.com>
2022-01-01 16:19:59 +0700

    Commit cuyy

---
+++ android/app/src/main/kotlin/com/example/uts_181021450009/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uts_181021450009
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "UTS 181021450009",
+            style: new TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        //backgroundColor: Colors.blue[100],
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.pink),
+                    child: const Center(
+                      child: Text(
+                        "pink",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.green),
+                    child: const Center(
+                      child: Text(
+                        "green",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.green),
+                    child: const Center(
+                      child: Text(
+                        "green",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.purple),
+                    child: const Center(
+                      child: Text(
+                        "purple",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.yellow),
+                    child: const Center(
+                      child: Text(
+                        "yellow",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.green),
+                    child: const Center(
+                      child: Text(
+                        "green",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.brown),
+                    child: const Center(
+                      child: Text(
+                        "brown",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.grey),
+                    child: const Center(
+                      child: Text(
+                        "grey",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0.0),
+                        ),
+                        color: Colors.green),
+                    child: const Center(
+                      child: Text(
+                        "green",
+                        style: TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
